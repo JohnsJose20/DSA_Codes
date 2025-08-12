ef7367bf390f8e072059f2d9eb42469b845dc7c8
Johns Jose <jjkanichukattu@gmail.com>
2025-08-12 08:13:45 +0530

    Add files via upload

---
+++ Queue.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int element) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+    } else {
+        if (front == -1)
+            front = 0;
+        rear++;
+        queue[rear] = element;
+        printf("Enqueued: %d\n", element);
+    }
+}
+
+void dequeue() {
+    if (front == -1 || front > rear) {
+        printf("Queue Underflow\n");
+    } else {
+        printf("Dequeued: %d\n", queue[front]);
+        front++;
+    }
+}
+
+void display() {
+    if (front == -1 || front > rear) {
+        printf("Queue is Empty\n");
+    } else {
+        printf("Queue Elements: ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
+    return 0;
+}
